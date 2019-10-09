8dfc15f9be6307dbec65ca71af6e8367aa7d00e9
VicaFebriyanti <53246321+VicaFebriyanti@users.noreply.github.com>
2019-10-09 08:52:47 +0700

    Add files via upload

---
+++ Latihan_1.php
@@ -0,0 +1,50 @@
+<?php
+//echo "Hello World !"; /*untuk menampilkan sebuah string*/
+//echo "<h1>Hello World !</h1>"; /*untuk menampilkan sebuah string*/
+
+// VARIABEL //
+// diawali dengan tanda $, ex = $nama 
+// nama variabel tidak boleh diawali dengan angka, ex = $11nama 
+// boleh mengandung angka
+
+//$nama = "Vica Febriyanti";
+//$pesan = "Halo, Selamat Datang <strong>$nama</strong>";
+//echo $pesan;
+
+// CONCATENATION / CONCAT / PENGGABUNGAN STRING //
+// .
+//$nama_depan = "Vica";
+//$nama_belakang = "Febriyanti";
+//echo $nama_depan . " " . $nama_belakang;
+
+// ASSIGNMENT / PENUGASAN //
+// =, +=, -=, *=, /=, %=, .=
+//$x = "Vica";
+//$x .= " ";
+//$x .= "Febriyanti";
+//echo $x;
+  //echo"<br>";
+//$angka = 10;
+//$angka += 5;
+//$angka += 3;
+//echo $angka;
+
+// OPERATOR //
+// Aritmatika 
+// + - * / %(modulus)
+
+//$a = 20;
+//$b = 10;
+//echo $a = $b;
+
+// PERBANDINGAN / COMPARATION //
+// ==, <=, >=, <, >, !=
+
+// IDENTITAS / STRICT COMPARATION // 
+// ===, !==
+
+// LOGIKA / LOGICAL //
+// &&, ||, !
+
+ 
+?>
---
+++ Latihan_2.php
@@ -0,0 +1,27 @@
+<?php
+// LOOPING / PENGULANGAN //
+// Type 1 = FOR
+//for( $i=0; $i <= 10; $i++) {
+    //echo $i . "Vica Febriyanti <br>";
+//}
+
+// Type 2 = WHILE 
+//$i = 1;
+//while ( $i <= 10 ) {
+    //echo $i . "Vica Febriyanti <br>";
+    //$i++;
+//}
+
+// Type 3 = FOREACH
+// Conditional / Pengkondisian
+$angka = 10;
+if ($angka > 10) {
+    echo "Lebih besar dari 10";
+}
+else if ($angka == 10) {
+    echo "Sama dengan 10";
+}
+else {
+    echo "Kurang dari 10";
+}
+?>
